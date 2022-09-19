0a2ce3f6966fa2b9690c011bb3db32c573b338d9
Cheng <113937942+Chengdada16@users.noreply.github.com>
2022-09-20 05:22:51 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oM6gy53jeiJCwgW7aw9DhR-ps3Gg",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "nAcVwacsbxL-gfKfyLiEjc4XsLP5_z57DD1IE_nVfo4",
+      useTemplateId: "f4MlmAQkvn0OWuYL2hUmBZTDfHPy0I2A1AsOrjPGsSI",
       // 所在省份
       province: "贵州",
       // 所在城市
